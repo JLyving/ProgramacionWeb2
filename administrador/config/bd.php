3136f52da6f28b2c6e6ef1e247c36e9c82737671
JLyving <juanpareye@gmail.com>
2023-05-23 22:06:25 -0500

    credenciales para webhost

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,5 +1,9 @@
 <?php
 // se conecta a la base de datos para ser utilizada
+//host = localhost
+//user = id20310731_juanitodb
+//pass = Ju@n59751812
+//name = id20310731_tes_php
 $host_db="localhost";
 $name_db="sitio";
 $user_db="root";
