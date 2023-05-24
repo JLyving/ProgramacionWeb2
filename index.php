8886ee2ebf107e91d828f92c423ddbb50611f143
JLyving <juanpareye@gmail.com>
2023-05-23 22:44:14 -0500

    index pagina principal

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include ("template/cabecera.php"); ?>
+
+            <div class="jumbotron text-center">
+                <h1 class="display-3">Bienvenidos a PerritosMotoricadosCo.</h1>
+                <p class="lead">Consulta los productos que tenemos</p>
+                <hr class="my-2">
+                <img width = "400" src = "img/PerroMotor.png" class="img-thumbnail rounded mx-auto d-block">
+
+                <p>Más información</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver Productos</a>
+                </p>
+            </div>    
+
+<?php include ("template/pie.php"); ?>
+
